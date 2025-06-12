bf80fbb377f2f585f7fed3b913cf022bf21d4eec
firetoss <hyz.firetoss@gmail.com>
2025-06-12 14:35:27 +0800

    modify

--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Portal,
   Box,
@@ -6,51 +8,67 @@ import {
   Flex,
   Text,
 } from "@chakra-ui/react"
+import { Tooltip } from "@/components/ui/tooltip"
 import { Link as RouterLink } from "@tanstack/react-router"
 import { useState } from "react"
 // import { useQueryClient } from "@tanstack/react-query"
 // import type { UserPublic } from "@/client"
 
-type exerciseType = "react" | "solid" | "vue" | "angular" | "svelte" | "preact"
+type exerciseType =
+  "mcq" |
+  "code" |
+  "basics" |
+  "db"
 
 const exerciseTypeList: { label: string, value: exerciseType }[] = [
-  { label: "Python编程题", value: "react" },
-  { label: "基础知识单选题", value: "solid" },
-  { label: "数据库单选题", value: "vue" },
-  { label: "程序算法单选题", value: "angular" },
-  { label: "Python单选题", value: "svelte" },
-  { label: "流程图单选题", value: "preact" },
+  { label: "算法与编程单选题", value: "mcq" },
+  { label: "Python程序编写", value: "code" },
+  { label: "基础知识单选题", value: "basics" },
+  { label: "数据库管理单选题", value: "db" },
 ]
 
 const exerciseConfigs = {
-  react: [
-    { name: "Python编程题", value: "react1", route: "/exercise/react1" },
-    { name: "Python编程题", value: "react2", route: "/exercise/react2" },
+  mcq: [
+    { type: "mcq", title: "同学们在日常数学学习中，如果遇到一道较为复杂的题目，一般会在认真审清题意的基础上，经过细致的分析推演，得到一个大致的解题思路，再把解题思路进一步细化分解为若干步骤，最后按计划好的步骤一步一步的进行推导和计算，最终完成试题的求解。其实，该思想同样适用于解决日常生活中遇到的问题。日常生活中的问题也需要在实施具体操作前，规划设计好解决问题的步骤，这样不仅能完美的解决问题，还能提高解决问题的效率。例如，骑电动车上学这一生活中的问题，就可以仿照上述思想进行。以下骑电动车上学问题的步骤中，需要在“插入钥匙启动电动车”之后执行的是（  ） ", content: "react1", route: "/exercise/mcq" },
+    { type: "mcq", title: "同学们在日常数学学习中，如果遇到一道较为复杂的题目，一般会在认真审清题意的基础上，经过细致的分析推演，得到一个大致的解题思路，再把解题思路进一步细化分解为若干步骤，最后按计划好的步骤一步一步的进行推导和计算，最终完成试题的求解。其实，该思想同样适用于解决日常生活中遇到的问题。日常生活中的问题也需要在实施具体操作前，规划设计好解决问题的步骤，这样不仅能完美的解决问题，还能提高解决问题的效率。例如，骑电动车上学这一生活中的问题，就可以仿照上述思想进行。以下骑电动车上学问题的步骤中，需要在“插入钥匙启动电动车”之后执行的是（  ） ", content: "react1", route: "/exercise/mcq" },
   ],
-  solid: [
-    { name: "Python编程题", value: "solid1", route: "/exercise" },
-    { name: "Python编程题", value: "solid2", route: "/exercise" },
+  code: [
+    {
+      type: "code", title: `打开素材文件夹中的python\test2.py，在该文件中完成程序。 
+一本故事书共120页，红红第一天看了a页，第二天看了b页，编程计算还剩多少页没有看。 
+输入 
+20
+30
+注：输入数据包含两行，第一行为a，第二行为b，两个数都为整数 
+输出 
+70 
+注：输出一个整数 。程序为：`, content: "solid1", route: "/exercise/code"
+    },
   ],
-  vue: [
-    { name: "Python编程题", value: "vue1", route: "/exercise" },
-    { name: "Python编程题", value: "vue2", route: "/exercise" },
+  basics: [
+    { type: "mcq", title: "基础知识选择题（对应知识点）", content: "vue1", route: "/exercise/basics" },
   ],
-  angular: [
-    { name: "angular1", value: "angular1", route: "/exercise" },
-    { name: "angular2", value: "angular2", route: "/exercise" },
-  ],
-  svelte: [
-    { name: "svelte1", value: "svelte1", route: "/exercise" },
-    { name: "svelte2", value: "svelte2", route: "/exercise" },
-  ],
-  preact: [
-    { name: "preact1", value: "preact1", route: "/exercise" },
-    { name: "preact2", value: "preact2", route: "/exercise" },
+  db: [
+    { type: "mcq", title: "数据库管理题库", content: "angular1", route: "/exercise/db" },
   ],
 }
 
 interface SidebarItemsProps {
   onClose?: () => void
+}
+
+function TitleToolTip({ title, index }: { title: string, index: number }) {
+  const [open, setOpen] = useState(false)
+  return (
+    <Tooltip
+      open={open}
+      positioning={{ placement: "left" }}
+      content={title.slice(0, 100) + "..."}
+      onOpenChange={({ open }) => setOpen(open)}
+    >
+      <Text ml={2} truncate>{(index + 1) + ". " + title}</Text>
+    </Tooltip>
+  )
 }
 
 const SidebarItems = ({ onClose }: SidebarItemsProps) => {
@@ -70,7 +88,7 @@ const SidebarItems = ({ onClose }: SidebarItemsProps) => {
 
   return (
     <>
-      <Box>
+      <Box maxW={300}>
         <Select.Root
           collection={collection}
           size="sm"
@@ -101,30 +119,29 @@ const SidebarItems = ({ onClose }: SidebarItemsProps) => {
           </Portal>
         </Select.Root>
         <Box mt={3}>
-          {/* <SimpleGrid columns={{ base: 1, sm: 1 }} > */}
           {(
-            exerciseItems.map((item) => (
+            exerciseItems.map((item, index) => (
               <RouterLink
-                key={item.value}
                 to={item.route}
-                onClick={onClose}>
+                onClick={onClose}
+                style={{ textDecoration: 'none' }}
+              >
                 <Flex
-                  key={item.value}
+                  key={index}
                   gap={4}
-                  px={4}
-                  py={2}
+                  // px={4}
+                  py={3}
                   alignItems="center"
-                  fontSize="sm"
+                  fontSize="md"
                   _hover={{
                     background: "gray.subtle",
                   }}
                 >
-                  <Text ml={2}>{item.name}</Text>
+                  <TitleToolTip title={item.title} index={index} />
                 </Flex>
               </RouterLink>
             ))
           )}
-          {/* </SimpleGrid> */}
         </Box>
       </Box>
     </>
