3ba908bf9f418540e854b8f940f4a223f0f6b19a
firetoss <hyz.firetoss@gmail.com>
2025-06-27 14:37:46 +0800

    modify

--- backend/app/api/routes/importer.py
+++ backend/app/api/routes/importer.py
@@ -22,25 +22,26 @@ def doc_import(session: SessionDep) -> Message:
     """
     导入题目。
     """
-    exercise_type = "code-python"
-    exercies = ExcrciseImporter.parse_doc(
-        f"app/exercise-original/{exercise_type}.docx", exercise_type
-    )
+    exercies = []
+    # exercise_type = "code-python"
+    # exercies = ExcrciseImporter.parse_doc(
+    #     f"app/exercise-original/{exercise_type}.docx", exercise_type
+    # )
 
-    for item in exercies:
-        exercie_in = Exercise()
-        exercie_in.id = uuid.uuid4()
-        exercie_in.type = exercise_type
-        exercie_in.category = "single"
-        exercie_in.question = json.dumps(item["question"])
-        exercie_in.options = (
-            "[]"
-            if exercise_type == "code-python"
-            else json.dumps(item["options"], ensure_ascii=False)
-        )
-        exercie_in.answer = json.dumps(item["answer"])
-        exercie = Exercise.model_validate(exercie_in)
-        session.add(exercie)
-        session.commit()
-        session.refresh(exercie)
+    # for item in exercies:
+    #     exercie_in = Exercise()
+    #     exercie_in.id = uuid.uuid4()
+    #     exercie_in.type = exercise_type
+    #     exercie_in.category = "single"
+    #     exercie_in.question = json.dumps(item["question"])
+    #     exercie_in.options = (
+    #         "[]"
+    #         if exercise_type == "code-python"
+    #         else json.dumps(item["options"], ensure_ascii=False)
+    #     )
+    #     exercie_in.answer = json.dumps(item["answer"])
+    #     exercie = Exercise.model_validate(exercie_in)
+    #     session.add(exercie)
+    #     session.commit()
+    #     session.refresh(exercie)
     return Message(message=f"题目导入成功({len(exercies)})")
