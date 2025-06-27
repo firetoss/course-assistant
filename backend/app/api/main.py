294519ca7bfeb917e43bd79f5eeb3aadcc47c73a
firetoss <hyz.firetoss@gmail.com>
2025-06-27 14:30:14 +0800

    modify

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -9,7 +9,7 @@ api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(items.router)
 api_router.include_router(exercise.router)
-api_router.include_router(importer.router)
+# api_router.include_router(importer.router)
 api_router.include_router(pic.router)
 
 
